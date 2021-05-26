592306c0f707bfad4ca21e51c16635dd9c9c234e
Matthew Casperson <matthewcasperson@gmail.com>
2021-05-26 15:09:56 +1000

    Integrated with Auth 0

--- src/main/java/org/matthewcasperson/GreetingResource.java
+++ src/main/java/org/matthewcasperson/GreetingResource.java
@@ -1,16 +1,24 @@
 package org.matthewcasperson;
 
+import io.quarkus.oidc.IdToken;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello-resteasy")
+@Path("/hello")
 public class GreetingResource {
+
+    @Inject
+    @IdToken
+    JsonWebToken idToken;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return "Hello " + this.idToken.getClaim("email");
     }
 }
