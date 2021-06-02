e73d6cd162a78d783f79c60001cad0ce3e6e788b
Matthew Casperson <matthewcasperson@gmail.com>
2021-06-02 16:38:49 +1000

    Fixed the test

--- src/test/java/org/matthewcasperson/GreetingResourceTest.java
+++ src/test/java/org/matthewcasperson/GreetingResourceTest.java
@@ -12,7 +12,7 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/")
           .then()
              .statusCode(403);
     }
